15242e63704338ba7b3320309823d243bba249b0
Mathieu Cloutier <mcloutier@coveo.com>
2021-10-26 13:05:14 -0400

    * added user input for regex and generalized the error message

--- packages/verify-changelogs/verify_changelogs.py
+++ packages/verify-changelogs/verify_changelogs.py
@@ -5,33 +5,40 @@ import sys
 import os
 from pathlib import Path
 
-# https://regex101.com/r/bIMRKq/1/
-CHANGELOG_FILE_PATTERN = r"^(?:- \*(?:feature|deprecation|fix|enhancement|refactor|internal|doc)\([\w\s]+\)\*: .+)+$"
-CHANGELOG_FILE_REGEX = re.compile(CHANGELOG_FILE_PATTERN)
+if __name__ == "__main__"
 
-files = glob.glob(os.environ["INPUT_CHANGELOGSLOCATION"])
+    # https://regex101.com/r/bIMRKq/1/
+    DEFAULT_CHANGELOG_FILE_PATTERN = r"^(?:- \*(?:feature|deprecation|fix|enhancement|refactor|internal|doc)\([\w\s]+\)\*: .+)+$"
+    
+    change_log_location = os.environ["INPUT_CHANGELOGSLOCATION"]
+    change_log_file_pattern = os.environ["INPUT_CHANGELOGFILEPATTERN"]
 
-invalid_lines = []
-has_lines = False
-for filename in files:
-    for number, line in enumerate(Path(filename).read_text().splitlines()):
-        has_lines = True
-        if not CHANGELOG_FILE_REGEX.match(line):
-            invalid_lines.append(f"{filename}:{number+1}: `{line}`")
+    if not change_log_file_pattern or change_log_file_pattern == "":
+        change_log_file_pattern = DEFAULT_CHANGELOG_FILE_PATTERN
+    change_log_file_regex = re.compile(change_log_file_pattern)
 
-if invalid_lines:
-    print(
-        f"The following changelogs do not match this pattern: {CHANGELOG_FILE_PATTERN}\n"
-        + "Here's a valid example: `- *fix(Scope)*: I fixed this [DT-1234]`\n\n    "
-        + "\n    ".join(invalid_lines)
-    )
-    print("\nIf you wish to make a multiline announcement, please do so in a message outside of the changelog")
-    sys.exit(1)
-elif not has_lines:
-    print(
-        "No changelogs were found. Please refer to the `unreleased-changes/INSTRUCTIONS.md` file or "
-        "add the following string to your pull request if this is not a user-facing change: `noChangelog=true`"
-    )
-    sys.exit(1)
-else:
-    print("All good!")
+    files = glob.glob(change_log_location)
+
+    invalid_lines = []
+    has_lines = False
+    for filename in files:
+        for number, line in enumerate(Path(filename).read_text().splitlines()):
+            has_lines = True
+            if not change_log_file_regex.match(line):
+                invalid_lines.append(f"{filename}:{number+1}: `{line}`")
+
+    if invalid_lines:
+        print(
+            f"The following changelogs do not match this pattern: {change_log_file_pattern}\n"
+            + "Here's a valid example: `- *fix(Scope)*: I fixed this [DT-1234]`\n\n    "
+            + "\n    ".join(invalid_lines)
+        )
+        print("\nIf you wish to make a multiline announcement, please do so in a message outside of the changelog")
+        sys.exit(1)
+    elif not has_lines:
+        print(
+            "No changelogs were found. Make sure a text file is present in '" + change_log_location "'."
+        )
+        sys.exit(1)
+    else:
+        print("All good!")
